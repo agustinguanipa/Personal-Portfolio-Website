f54512ee0dd11c20fc2b3cfdb3198ae657c70ae2
Agustin Guanipa <agustin_guanipa@hotmail.com>
2019-01-28 17:49:21 -0400

    Corrigiendo contacto.js

--- js/contacto.js
+++ js/contacto.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $('.contact-form').bootstrapValidator({
         message: 'This value is not valid',
         feedbackIcons: {
-            valid: 'fa fa-check',
+            valid: 'glyphicon glyphicon-ok',
             invalid: 'glyphicon glyphicon-remove',
             validating: 'glyphicon glyphicon-refresh'
         },
@@ -18,10 +18,21 @@ $(document).ready(function() {
                         max: 30,
                         message: 'The username must be more than 6 and less than 30 characters long'
                     },
-                    regexp: {
-                        regexp: /^[a-zA-Z0-9_]+$/,
-                        message: 'The username can only consist of alphabetical, number and underscore'
-                    }
+                   
+                }
+            },
+            apellidos: {
+                message: 'The username is not valid',
+                validators: {
+                    notEmpty: {
+                        message: 'The username is required and cannot be empty'
+                    },
+                    stringLength: {
+                        min: 6,
+                        max: 30,
+                        message: 'The username must be more than 6 and less than 30 characters long'
+                    },
+                   
                 }
             },
             email: {
@@ -32,6 +43,20 @@ $(document).ready(function() {
                     emailAddress: {
                         message: 'The input is not a valid email address'
                     }
+                }
+            },
+            mensaje: {
+                message: 'The username is not valid',
+                validators: {
+                    notEmpty: {
+                        message: 'The username is required and cannot be empty'
+                    },
+                    stringLength: {
+                        min: 6,
+                        max: 30,
+                        message: 'The username must be more than 6 and less than 30 characters long'
+                    },
+                   
                 }
             }
         }
