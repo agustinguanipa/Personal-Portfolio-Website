654ad190705c7d92c030c9323605f4743b1b59aa
Agustin Guanipa <agustin_guanipa@hotmail.com>
2019-04-22 20:25:50 -0400

    Arreglo en el index.php

--- index.php
+++ index.php
@@ -10,8 +10,8 @@
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="css/iconos.css">
 	<link rel="stylesheet" href="bootstrap-4.1.3-dist/css/bootstrap.min.css"/>
-  	<script src="https://code.jquery.com/jquery-3.1.0.min.js"></script>
-  	<script src="bootstrap-4.1.3-dist/js/bootstrap.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.1.0.min.js"></script>
+  <script src="bootstrap-4.1.3-dist/js/bootstrap.min.js"></script>
 </head>
 
 <body>
